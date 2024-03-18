b76306801efd601cb7d9f6048c8f5992a0605c39
Alexis BRIET <alexis.briet2003@gmail.com>
2024-03-18 11:37:53 +0100

    Add tailwind and flowbite

---
+++ src/components/Topbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 v-if="title != null" class="text-4xl font-extrabold dark:text-white pt-10 px-10">{{title}}</h1>
+    <p v-if="subtitle != null" class="mb-6 text-lg font-normal text-gray-500 dark:text-gray-400 pt-5 px-10">{{ subtitle }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, defineProps } from 'vue'
+
+defineComponent({
+  name: 'TopbarForPage'
+})
+
+defineProps({
+  title: String,
+  subtitle: String
+})
+</script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <h1>Mon profil</h1>
+    <Topbar title="Mon profil" subtitle="test" />
   </div>
 </template>
+<script setup lang="ts">
+import Topbar from '@/components/Topbar.vue'
+</script>
