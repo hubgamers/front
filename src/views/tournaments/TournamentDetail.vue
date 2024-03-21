4bdff046b5fa414ab01ccc2e43621606c515f7c0
Alexis BRIET <alexis.briet2003@gmail.com>
2024-03-21 09:39:37 +0100

    Add moment and dates tournamentDetail

--- src/views/tournaments/TournamentDetail.vue
+++ src/views/tournaments/TournamentDetail.vue
@@ -10,8 +10,9 @@
         <span class="max-w-[5rem] bg-blue-100 text-blue-800 text-base font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"><i class="fa-solid fa-gamepad"></i> {{ store.getters.getTournament.game }}</span>
         <span class="max-w-[5rem] bg-blue-100 text-blue-800 text-base font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"><i class="fa-solid fa-laptop"></i> {{ store.getters.getTournament.platform }}</span>
       </div>
-
-
+      
+      <p>Début : {{ formatDate(store.getters.getTournament.startDate, 'DD/MM/YYYY hh:mm')  }}</p>
+      <p>Fin : {{ formatDate(store.getters.getTournament.endDate, 'DD/MM/YYYY hh:mm')  }}</p>
     </div>
   </div>
 </template>
@@ -21,6 +22,7 @@ import CardComponent from '@/components/CardComponent.vue'
 import { defineComponent } from 'vue'
 import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
+import moment from 'moment/moment'
 
 defineComponent({
   name: 'TournamentDetailPage'
@@ -30,5 +32,9 @@ const store = useStore();
 const params = useRoute().params;
 if (params && params.id) {
   store.dispatch('getTournamentById', params.id)
+}
+
+function formatDate(date: any, formatDate= 'yyyy-MM-dd') {
+  return moment(date).format(formatDate)
 }
 </script>
