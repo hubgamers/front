cc252387ddfa4e3d9757b36a8de363b5989f2d46
Alexis BRIET <alexis.briet2003@gmail.com>
2024-03-21 21:18:22 +0100

    Update vitrine pages

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,79 +1,68 @@
 <template>
-  <div class="navbar">
-    <div>
-      <img class="max-w-[80px]" src="https://via.placeholder.com/50" alt="logo" />
-      <h1>HubGamers</h1>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <RouterLink to="/">
-            Accueil
-          </RouterLink>
-        </li>
-        <li class="dropdown">
-          <a href="#" @click="toggleDropdown">Services</a>
-          <ul v-show="showDropdown">
-            <li>
-              <RouterLink to="/tournament">Tournoi</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/teams">Equipe</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/player">Joueur</RouterLink>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <RouterLink to="/contact">
-            Contact
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/auth/login">
-            <ButtonDark>Se connecter</ButtonDark>
-          </RouterLink>
-        </li>
-      </ul>
+  <header class="absolute inset-x-0 top-0 z-50">
+    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <a href="#" class="-m-1.5 p-1.5">
+          <span class="sr-only">Hub Gamers</span>
+          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+        </a>
+      </div>
+      <div class="flex lg:hidden">
+        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+          <span class="sr-only">Open main menu</span>
+          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </button>
+      </div>
+      <div class="hidden lg:flex lg:gap-x-12">
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Fonctionnalités</a>
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Tarifs</a>
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Plateformes</a>
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">A propos</a>
+      </div>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <RouterLink to="/auth/login" class="text-sm font-semibold leading-6 bg-blue-700 px-2 py-1  text-gray-900">Se connecter <span aria-hidden="true">&rarr;</span></RouterLink>
+      </div>
     </nav>
-  </div>
+    <!-- Mobile menu, show/hide based on menu open state. -->
+    <div class="lg:hidden" role="dialog" aria-modal="true">
+      <!-- Background backdrop, show/hide based on slide-over state. -->
+      <div class="fixed inset-0 z-50"></div>
+      <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Hub Gamers</span>
+            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+          </a>
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+            <span class="sr-only">Close menu</span>
+            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Fonctionnalités</a>
+              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Tarifs</a>
+              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Plateformes</a>
+              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">A propos</a>
+            </div>
+            <div class="py-6">
+              <RouterLink to="/auth/login" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Se connecter</RouterLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
-<style>
-.navbar {
-  display: flex;
-  justify-content: space-around;
-}
-.navbar div {
-  display: flex;
-  gap: 1rem;
-  align-items: center;
-}
-nav ul {
-  display: flex;
-  gap: 3rem;
-  align-items: center;
-  padding: 1rem;
-}
-.dropdown ul {
-  display: flex;
-  flex-flow: column;
-  gap: 1rem;
-  padding: 1rem;
-  position: absolute;
-  margin-top: 1rem;
-  background: #dcdcdc;
-  max-width: 150px;
-}
-</style>
 <script setup lang="ts">
+import { defineComponent } from 'vue'
 
-import ButtonDark from '@/components/ButtonDark.vue'
-import { ref } from 'vue'
-
-let showDropdown = ref(false);
-
-function toggleDropdown() {
-  showDropdown.value = !showDropdown.value;
-}
+defineComponent({
+  name: 'NavbarComponent',
+})
 </script>
--- src/views/dashboard/teams/Teams.vue
+++ src/views/dashboard/teams/Teams.vue
@@ -9,32 +9,38 @@
 
       <div v-if="invitationModal" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
         <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <form @submit.prevent="sendInvitation" class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all">
               <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                <div class="sm:flex sm:items-start">
+                <div class="flex flex-col gap-1">
+                  <form @submit.prevent="searchPlayer" class="flex flex-row">
+                    <div class="flex flex-col">
+                      <label for="playerUsername">Rechercher un joueur</label>
+                      <input v-model="playerUsername" type="text" name="playerUsername" id="playerUsername">
+                    </div>
+                    <button type="submit" class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Rechercher un joueur</button>
+                  </form>
+                  <form>
                     <div>
                       <label for="playerId">Sélectionner un joueur</label>
-                      <select v-model="invitationForm.playerId" name="playerId" id="playerId">
-                        <option v-for="(player, playerIndex) in store.getters.getPlayers" :key="playerIndex" :value="player.id">{{player.username}}</option>
+                      <input v-model="playerUsername" type="text" name="playerId" id="playerId" disabled>
+                    </div>
+                    <div>
+                      <label for="teamId">Sélectionner une équipe</label>
+                      <select v-model="invitationForm.teamId" name="teamId" id="teamId">
+                        <option v-for="(team, teamIndex) in store.getters.getTeams" :key="teamIndex" :value="team.id">{{team.name}}</option>
                       </select>
                     </div>
-                  <div>
-                    <label for="teamId">Sélectionner une équipe</label>
-                    <select v-model="invitationForm.teamId" name="teamId" id="teamId">
-                      <option v-for="(team, teamIndex) in store.getters.getTeams" :key="teamIndex" :value="team.id">{{team.name}}</option>
-                    </select>
-                  </div>
+                  </form>
                 </div>
               </div>
               <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                <button type="submit" class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Envoyer l'invitation</button>
+                <button type="submit" @click="sendInvitation" class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Envoyer l'invitation</button>
                 <button type="button" @click="toggleInvitationModal" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Annuler</button>
               </div>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
@@ -72,6 +78,12 @@ let invitationForm = ref({
   teamId: null,
   type: 'RECRUIT_PLAYER'
 })
+
+let playerUsername = ref('')
+function searchPlayer() {
+  store.dispatch('getPlayerByUsername', playerUsername.value)
+  invitationForm.value.playerId = store.getters.getPlayerByUsername.id
+}
 
 function toggleInvitationModal() {
   invitationModal.value = !invitationModal.value
