9d7be3452d716b1247cefd48cf2a742af9e446f3
Alexis BRIET <alexis.briet2003@gmail.com>
2024-04-24 21:10:41 +0200

    Add new font and input component

---
+++ src/components/InputText.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <label>{{ label }} <span v-if="required" class="required">*</span></label>
+    <input :type="type" :placeholder="placeholder" v-model="model">
+    <p class="info">{{info}}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    label: {
+      type: String,
+      default: 'Label'
+    },
+    placeholder: {
+      type: String,
+      default: 'Placeholder'
+    },
+    model: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    info: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-flow: column;
+  gap: .3rem;
+}
+label {
+  color: #6F7482;
+  font-size: 16px;
+}
+span.required {
+  color: #ED0131;
+}
+input {
+  background-color: #F8FAFC;
+  padding: .5rem 1rem;
+  border: none;
+}
+input::placeholder {
+  color: #B8BCCA;
+}
+p.info {
+  color: #1b9cf8;
+  font-size: 14px;
+  margin: 0;
+}
+</style>
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,86 +1,23 @@
 <template>
-  <section class="bg-gray-50 dark:bg-gray-900">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-        <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-        Flowbite
-      </a>
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 v-if="isLogin" class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Se connecter à votre compte</h1>
-          <h1 v-else class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Se créer un compte</h1>
-          <form class="space-y-4 md:space-y-6" v-if="isLogin" @submit.prevent="login">
-            <div>
-              <label for="login" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Identifiant</label>
-              <input v-model="auth.login" type="text" name="login" id="login" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required>
-            </div>
-            <div>
-              <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-              <input v-model="auth.password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-            </div>
-            <div class="flex items-center justify-between">
-              <div class="flex items-start">
-                <div class="flex items-center h-5">
-                  <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required>
-                </div>
-                <div class="ml-3 text-sm">
-                  <label for="remember" class="text-gray-500 dark:text-gray-300">Se souvenir de moi</label>
-                </div>
-              </div>
-              <RouterLink to="/auth/forgot" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Mot de passe oublié</RouterLink>
-            </div>
-            <button type="submit" class="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Se connecter</button>
-            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-              Pas encore de compte ? <RouterLink to="/auth/register" class="font-medium text-primary-600 hover:underline dark:text-primary-500">S'inscrire</RouterLink>
-            </p>
-          </form>
-
-          <form class="space-y-4 md:space-y-6" v-if="!isLogin" @submit.prevent="register">
-            <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adresse mail</label>
-              <input v-model="auth.email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required>
-            </div>
-            <div>
-              <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom d'utilisateur</label>
-              <input v-model="auth.username" type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="JohnDoe80" required>
-            </div>
-            <div>
-              <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-              <input v-model="auth.password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-            </div>
-            <div class="flex items-center justify-between">
-              <div class="flex items-start">
-                <div class="flex items-center h-5">
-                  <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required>
-                </div>
-                <div class="ml-3 text-sm">
-                  <label for="remember" class="text-gray-500 dark:text-gray-300">Se souvenir de moi</label>
-                </div>
-              </div>
-              <RouterLink to="/auth/forgot" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Mot de passe oublié</RouterLink>
-            </div>
-            <button type="submit" class="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">S'inscrire</button>
-            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-              Déjà un compte ? <RouterLink to="/auth/login" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Se connecter</RouterLink>
-            </p>
-          </form>
-        </div>
-      </div>
+  <div class="containerAuth">
+    <h1>Créer un compte</h1>
+    <div class="containerForm">
+      <input-text v-model="auth.username" type="string" label="Nom d'utilisateur" placeholder="Nom d'utilisateur" :required="true" />
+      <input-text v-model="auth.email" type="string" label="Email" placeholder="Email" :required="true" />
+      <input-text v-model="auth.password" type="password" label="Mot de passe" placeholder="•••••••••" :required="true" />
     </div>
-  </section>
+    <button-dark @click="register">Créer un compte</button-dark>
+  </div>
 </template>
 <script setup lang="ts">
-import Topbar from '@/components/Topbar.vue'
 import { defineComponent, ref, watchEffect } from 'vue'
 import { useStore } from 'vuex'
 import router from '@/router'
 import { useRoute } from 'vue-router'
 import ButtonDark from '@/components/ButtonDark.vue'
+import InputText from '@/components/InputText.vue'
 defineComponent({
   name: 'AuthentificationPage',
-  components: {
-    Topbar
-  }
 })
 
 const store = useStore();
@@ -116,3 +53,23 @@ function login() {
   })
 }
 </script>
+
+<style scoped>
+.containerAuth {
+  max-width: 1400px;
+  margin: 0 auto;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.containerForm {
+  display: flex;
+  flex-flow: column;
+  gap: 3rem;
+  min-width: 600px;
+  max-width: 300px;
+}
+</style>
