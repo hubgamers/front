48acb5d39816ae9d62f6e9f49bfaaba29ad5a6ef
Alexis BRIET <alexis.briet2003@gmail.com>
2024-06-12 12:21:18 +0200

    Add card on dashboard

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,13 +1,40 @@
 <template>
   <DashboardLayout title="Tableau de bord" subtitle="Bienvenue sur votre compte" :show-search-bar="true">
-    
+   <div class="row justify-around gap-2 pt-2">
+     <CardActionDashboardComponent
+        title="Equipes créées"
+        subtitle="10"
+        btnUri="/dashboard/my-teams"
+        btnText="Voir mes équipes"
+        bg-color="blue"
+     />
+     <CardActionDashboardComponent
+       title="Tournois crées"
+       subtitle="23"
+       btnUri="/dashboard/tournaments"
+       btnText="Voir mes tournois"
+       bg-color="blue"
+     />
+     <CardActionDashboardComponent
+       title="Status d'abonnement"
+       subtitle="Platinum"
+       btnUri="/dashboard/subscriptions"
+       btnText="Voir mon abonnement"
+       bg-color="green"
+       type="large"
+     />
+   </div>
   </DashboardLayout>
 </template>
 
 <script setup lang="ts">
 import { defineComponent } from 'vue'
 import DashboardLayout from '@/layout/DashboardLayout.vue'
+import CardActionDashboardComponent from '@/views/dashboard/components/CardDashboardComponent.vue'
 defineComponent({
   name: 'DashboardPage'
 })
 </script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/dashboard/components/CardDashboardComponent.vue
@@ -0,0 +1,72 @@
+<template>
+  <div :class="'card ' + 'bg-' + bgColor + ' ' + (type === '' ? 'w300px' : 'wfull')">
+    <div>
+      <h2>{{title}}</h2>
+      <span>{{subtitle}}</span>
+    </div>
+
+    <div class="action">
+      <router-link :to="btnUri" class="btn btn-primary">{{btnText}}</router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineComponent } from 'vue'
+
+defineComponent({
+  name: 'CardDashboardComponent'
+})
+
+defineProps({
+  title: String,
+  subtitle: String,
+  type: String,
+  bgColor: String,
+  btnUri: String,
+  btnText: String
+})
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border-radius: 20px;
+
+  &.wfull {
+    width: 100%;
+  }
+  &.w300px {
+    width: 300px;
+  }
+
+  div:first-of-type {
+    padding: 1rem;
+  }
+
+  h2, span, a {
+    color: #FFFFFF;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    margin-bottom: 0.5rem;
+  }
+  span {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
+  .action {
+    border-top: 2px solid #FFFFFF;
+    padding: 1rem;
+    cursor: pointer;
+    transition: all .5s;
+
+    &:hover {
+      transition: all .5s;
+      background: #9DD9D2;
+      border-radius: 0 0 20px 20px;
+    }
+  }
+}
+</style>
