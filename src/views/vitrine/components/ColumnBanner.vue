3ed7b69da2ae0adda6c227880817f3b1c08fd9bb
Alexis BRIET <alexis.briet2003@gmail.com>
2024-06-12 10:38:24 +0200

    Update feats page

---
+++ src/views/vitrine/components/ColumnBanner.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="column_banner_feat">
+    <h4>{{title}}</h4>
+    <p>{{content}}</p>
+    <button class="green">{{buttonText}}</button>
+  </div>
+</template>
+
+<script setup>
+import { defineComponent } from 'vue'
+
+defineComponent({
+  name: 'ColumnBannerComponent'
+})
+
+defineProps({
+  title: String,
+  content: String,
+  buttonText: String
+})
+</script>
+
+<style lang="scss">
+.column_banner_feat {
+  display: flex;
+  flex-flow: column;
+  background-image: url("../../../../public/bg-color-red.jpg");
+  background-size: cover;
+  padding: 4rem 2rem;
+  border-radius: 15px;
+  margin: 2rem;
+  height: 100%;
+
+  h4 {
+    font-size: 2rem;
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
+  p {
+    max-width: 400px;
+    margin-bottom: 3rem;
+  }
+}
+</style>
