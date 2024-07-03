1592404c7b4152735773c149be3a642af5b2c482
Alexis BRIET <alexis.briet2003@gmail.com>
2024-07-03 11:20:17 +0200

    Add TeamRoster service

---
+++ src/services/teamRosterService.js
@@ -0,0 +1,37 @@
+import axiosCustom from '@/services/axiosCustom.js'
+
+export default {
+  getAdminColumns() {
+    return axiosCustom.get("/admin/teamRoster/columns");
+  },
+  getColumns() {
+    return axiosCustom.get("/teamRoster/columns");
+  },
+  getAllMyTeams() {
+    return axiosCustom.get("/teamRoster/my-teams");
+  },
+  countAllTeams() {
+    return axiosCustom.get("/teamRoster/count-all");
+  },
+  getAllPublicTeams () {
+    return axiosCustom.get("/teamRoster/all-public");
+  },
+  getAllTeamsByName(name) {
+    return axiosCustom.get(`/teamRoster/all/name/${name}`);
+  },
+  getTeamByName(name) {
+    return axiosCustom.get(`/teamRoster/name/${name}`);
+  },
+  getTeamById(id) {
+    return axiosCustom.get(`/teamRoster/id/${id}`);
+  },
+  createTeam(team) {
+    return axiosCustom.post("/teamRoster/create", team);
+  },
+  updateTeam(team) {
+    return axiosCustom.put("/teamRoster/update", team);
+  },
+  deleteTeam(id) {
+    return axiosCustom.delete(`/teamRoster/delete/${id}`);
+  }
+}
