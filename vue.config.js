977f71764ab1da4da1c88f38b32f61771414f688
Alexis BRIET <alexis.briet2003@gmail.com>
2024-07-11 22:29:29 +0200

    Add docker compose to use network

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/api',
+        target: 'http://spring-boot-app:8080/api',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
