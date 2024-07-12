92e6b5636b4cbafe45676c565a77862e7211cd0e
Alexis BRIET <alexis.briet2003@gmail.com>
2024-07-12 16:07:44 +0200

    Add Responsive on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,35 @@
 <template>
   <header>
     <div class="menu">
-      <h2>HubGamers</h2>
-      <nav>
+      <h2 :class="{ 'is-open': isMenuOpen }">HubGamers</h2>
+      <button class="menu-toggle" @click="toggleMenu">
+        <i class="fa fa-bars"></i>
+      </button>
+      <nav :class="{ 'is-open': isMenuOpen }">
         <ul>
           <li>
-            <RouterLink to="/">Accueil</RouterLink>
+            <RouterLink to="/" @click="closeMenu">Accueil</RouterLink>
           </li>
           <li>
-            <RouterLink to="/features">Fonctionnalités</RouterLink>
+            <RouterLink to="/features" @click="closeMenu">Fonctionnalités</RouterLink>
           </li>
           <li>
-            <RouterLink to="/pricing">Tarifs</RouterLink>
+            <RouterLink to="/pricing" @click="closeMenu">Tarifs</RouterLink>
           </li>
           <li>
-            <RouterLink to="/about">A propos</RouterLink>
+            <RouterLink to="/about" @click="closeMenu">A propos</RouterLink>
           </li>
           <li>
-            <RouterLink to="/contact">Contactez-nous</RouterLink>
+            <RouterLink to="/contact" @click="closeMenu">Contactez-nous</RouterLink>
+          </li>
+          <li class="is-mobile-link" v-if="!isConnected">
+            <RouterLink to="/auth/login" @click="closeMenu">Connexion</RouterLink>
+          </li>
+          <li class="is-mobile-link" v-if="!isConnected">
+            <RouterLink to="/auth/register" @click="closeMenu">Inscription</RouterLink>
+          </li>
+          <li class="is-mobile-link" v-if="isConnected">
+            <RouterLink to="/dashboard" @click="closeMenu">Tableau de bord</RouterLink>
           </li>
         </ul>
       </nav>
@@ -52,6 +64,7 @@
     </div>
   </header>
 </template>
+
 <script setup>
 import { defineComponent, ref } from 'vue'
 
@@ -60,6 +73,8 @@ defineComponent({
 })
 
 let isConnected = ref(false)
+let isMenuOpen = ref(false)
+
 if (localStorage.getItem('userId')) {
   isConnected.value = true
 }
@@ -67,6 +82,14 @@ if (localStorage.getItem('userId')) {
 function logout() {
   localStorage.clear();
   window.location.reload();
+}
+
+function toggleMenu() {
+  isMenuOpen.value = !isMenuOpen.value;
+}
+
+function closeMenu() {
+  isMenuOpen.value = false;
 }
 </script>
 
@@ -77,33 +100,51 @@ header {
   justify-content: space-between;
   padding: 1rem 2rem;
   margin-bottom: 5rem;
+
   .menu {
     display: flex;
     align-items: center;
     gap: 2rem;
-    
+
     h2 {
       color: #FFFFFF;
       font-size: 1.5rem;
       font-weight: 700;
       margin: inherit;
+      &.is-open {
+        display: none;
+      }
     }
-    
-    nav ul {
-      display: flex;
-      gap: 2rem;
-      
-      li a {
-        font-weight: 700;
-        color: #FFFFFF;
-        
-        &:hover, &:focus, &:active {
-          color: #9DD9D2;
+
+    .menu-toggle {
+      display: none;
+      background: none;
+      border: none;
+      color: #FFFFFF;
+      font-size: 1.5rem;
+    }
+
+    nav {
+      ul {
+        display: flex;
+        gap: 2rem;
+
+        li a {
+          font-weight: 700;
+          color: #FFFFFF;
+
+          &:hover, &:focus, &:active {
+            color: #9DD9D2;
+          }
         }
+      }
+
+      &.is-open {
+        display: block;
       }
     }
   }
-  
+
   .actions {
     display: flex;
     gap: 2rem;
@@ -115,17 +156,63 @@ header {
         color: #FFF;
       }
     }
-    
+
     ul {
       display: flex;
       gap: 1rem;
-      
+
       li a i {
         color: #FFFFFF;
-        
+
         &:hover, &:focus, &:active {
           color: #9DD9D2;
         }
+      }
+    }
+  }
+  
+  .is-mobile-link {
+    display: none;
+    @media screen and (max-width: 768px){
+      display: block;
+    }
+  }
+
+  @media (max-width: 768px) {
+    .menu {
+      display: flex;
+      flex-flow: row;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+
+      .menu-toggle {
+        display: block;
+      }
+
+      nav {
+        display: none;
+        width: 100%;
+        ul {
+          flex-direction: column;
+          gap: 1rem;
+          li {
+            width: 100%;
+            a {
+              display: block;
+              padding: 0.5rem 0;
+            }
+          }
+        }
+      }
+    }
+
+    .actions {
+      display: none;
+      @media screen and (min-width: 768px){
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
       }
     }
   }
