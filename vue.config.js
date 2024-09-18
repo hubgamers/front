83bf8d9d5f1f837b9734a078504b8cac1fdeb6b5
Alexis BRIET <alexis.briet2003@gmail.com>
2024-09-18 19:18:14 +0200

    Update vue

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/api',
+        target: 'https://localhost:8080/api',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
