2bd2704afb0bf25b62a08ca3e5ad5126171bcc45
Alexis BRIET <alexis.briet2003@gmail.com>
2024-10-01 16:37:44 +0200

    Add SEO

--- src/router/index.js
+++ src/router/index.js
@@ -20,28 +20,48 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/vitrine/Home.vue')
+    component: () => import('@/views/vitrine/Home.vue'),
+    meta: {
+      title: 'Accueil',
+      description: "HubGamers est la plateforme ultime pour créer et gérer des structures de gaming. Formez des équipes pour vos jeux et plateformes préférés, organisez des tournois, et défiez d'autres équipes dans un environnement compétitif et social"
+    }
   },
   {
     path: '/features',
     name: 'Features',
-    component: () => import('@/views/vitrine/Features.vue')
+    component: () => import('@/views/vitrine/Features.vue'),
+    meta: {
+      title: 'Fonctionnalités - HubGamers',
+      description: 'Explorez les fonctionnalités puissantes de HubGamers, la plateforme idéale pour créer des structures de gaming, organiser des tournois et affronter des équipes adverses.'
+    }
   },
   {
     path: '/pricing',
     name: 'Pricing',
-    component: () => import('@/views/vitrine/Pricing.vue')
+    component: () => import('@/views/vitrine/Pricing.vue'),
+    meta: {
+      title: 'Tarifs - HubGamers',
+      description: 'Découvrez nos plans tarifaires flexibles et adaptés pour toutes les structures de gaming sur HubGamers. Choisissez l\'offre qui correspond à vos besoins.'
+    }
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('@/views/vitrine/About.vue')
+    component: () => import('@/views/vitrine/About.vue'),
+    meta: {
+      title: 'À propos - HubGamers',
+      description: 'Apprenez-en plus sur l\'équipe passionnée derrière HubGamers et notre mission de révolutionner la gestion de structures et d\'équipes dans le monde du gaming.'
+    }
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import('@/views/vitrine/Contact.vue')
-  },
+    component: () => import('@/views/vitrine/Contact.vue'),
+    meta: {
+      title: 'Contact - HubGamers',
+      description: 'Besoin d\'aide ou de plus d\'informations ? Contactez l\'équipe de HubGamers et obtenez des réponses rapides à toutes vos questions.'
+    }
+  },  
   {
     path: '/structures',
     name: 'Structures',
@@ -55,24 +75,38 @@ const routes = [
   {
     path: '/legals',
     name: 'Legals',
-    component: () => import('@/views/vitrine/Legals.vue')
+    component: () => import('@/views/vitrine/Legals.vue'),
+    meta: {
+      title: 'Mentions légales'
+    }
   },
   {
     path: '/cgu',
     name: 'CGU',
-    component: () => import('@/views/vitrine/CGU.vue')
+    component: () => import('@/views/vitrine/CGU.vue'),
+    meta: {
+      title: 'Conditions générales d\'utilisation'
+    }
   },
   ...authModule,
   {
     path: '/dashboard',
     name: 'dashboard',
-    component: () => import('@/views/dashboard/Dashboard.vue')  // Assurez-vous que ce chemin est correct
+    component: () => import('@/views/dashboard/Dashboard.vue'),
+    meta: {
+      title: 'Tableau de bord',
+      description: 'Gérez vos structures, équipes, tournois et défis depuis le tableau de bord HubGamers. Suivez vos performances et connectez-vous avec la communauté gaming.'
+    }
   },
   ...subscriptionModule,
   {
     path: '/dashboard/profile',
     name: 'Profile',
-    component: () => import('@/views/dashboard/Profile.vue')
+    component: () => import('@/views/dashboard/Profile.vue'),
+    meta: {
+      title: 'Profil',
+      description: 'Modifiez votre profil. Créez votre structure esportive et recrutez des joueurs pour vos tournois et matchs amicaux.'
+    }
   },
   ...structureModule,
   ...playersModule,
